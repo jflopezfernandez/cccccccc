7a5ffa4cc4beacb71917f18fd0fb5e44108ec181
lopezfjose <jflopezfernandez@gmail.com>
2017-06-05 10:20:53 -0400

    Push

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 
 #include "includes/Main.hpp"
 
+class ComplexNumber {
+    public:
+        int real;
+        int comp;
+};
+
+class Point {
+    public:
+        ComplexNumber   x,
+                        y,
+                        z;
+};
+
 
 
 int main()
 {
     initializeProgram();
-
-    //...
 
     terminateProgram();
     return EXIT_SUCCESS;
